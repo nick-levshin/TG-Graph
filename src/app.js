aa4057141aacc7c47e3ed4805a93e0da20e6c74b
Nick Levshin <nikita.levshin2016@gmail.com>
2021-12-18 20:27:00 +0300

    added data.json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { chart } from './chart';
-import { getChartData } from './data';
+import data from './data.json';
 import './styles.scss';
 
-const tgChart = chart(document.getElementById('chart'), getChartData());
+const tgChart = chart(document.getElementById('chart'), data[4]);
 tgChart.init();
